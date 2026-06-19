ddb2b1f62ac1bad309f055d502d323e08f47f316
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-19 16:05:59 +0600

    Add Java program to reverse an integer

---
+++ rev.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 1234;
+        int rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Reversed = " + rev);
+    }
+}
