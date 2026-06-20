d007f5eb7447d4c9de1b8f7e55aa8483e4159a43
JAMSAD CHOWDHURY <jamsad241-15-028@diu.edu.bd>
2026-06-20 20:20:20 +0600

    Add Main class to print array elements

---
+++ nn.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] niha = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < niha.length; i++) {
+            System.out.println(niha[i]);
+        }
+    }
+}
